3187d8045c2fae5cb1cfe1754cc0031170161567
Alastair Paterson <alas.paterson@gmail.com>
2016-05-01 21:16:27 +0100

    Hooked up initial test entity, component, and system.
    Added swp to gitignore
    
    Updated makefile

--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,11 @@
 #include <ncurses.h>
 #include <vector>
 #include <time.h>
-#include "room.h"
 #include "level.h"
+#include "render_system.h"
+#include "entity.h"
+#include "graphic_component.h"
+#include "location_component.h"
 
 int main(int argc, char** argv)
 {
@@ -14,17 +17,22 @@ int main(int argc, char** argv)
 	noecho();
 	raw();
 
-	//int mapx, mapy;
+	Entity e(0);
+	LocationComponent lc(10, 10);
+	GraphicComponent  gc('@');
+	
+	e.add_component(&lc);
+	e.add_component(&gc);
 
-	//getmaxyx(stdscr, mapy, mapx);
+	RenderSystem r_sys;
+	r_sys.add_entity(&e);
 
 	Level l;
 	l.load("sira_field");
 	l.draw();
 
-	//mvprintw(0, 0, "Scr x: %d", mapx);
-	//mvprintw(1, 0, "Scr y: %d", mapy);
-	
+	r_sys.update();
+
 	refresh();
 	getch();
 	
