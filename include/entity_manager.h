24d87f8f4b10eab73d87e2a7831d05b846e8b504
Alastair Paterson <alas.paterson@gmail.com>
2016-06-07 00:38:58 +0100

    Updated Entity Manager to be a singleton

--- include/entity_manager.h
+++ include/entity_manager.h
@@ -8,12 +8,21 @@
 class EntityManager
 {
 public:
-	EntityManager(void);
-	~EntityManager(void);
+	
+	static EntityManager& get_instance(void)
+	{
+		static EntityManager instance;
+
+		return instance;
+	}
+
+	EntityManager(EntityManager const&)  = delete;
+	void operator=(EntityManager const&) = delete;
 
 	std::shared_ptr<Entity> create_entity(void);
 
 private:
+	EntityManager(void) {};
 	std::map<int, std::shared_ptr<Entity>> _entities;
 
 	static int NEXT_ID;
--- src/entity_manager.cpp
+++ src/entity_manager.cpp
@@ -2,10 +2,6 @@
 
 int EntityManager::NEXT_ID = 0;
 
-EntityManager::EntityManager(void) {}
-
-EntityManager::~EntityManager(void) {}
-
 std::shared_ptr<Entity> EntityManager::create_entity(void)
 {
 	std::shared_ptr<Entity> eptr = std::make_shared<Entity>(NEXT_ID);
