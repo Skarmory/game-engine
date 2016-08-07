49e1d54cf8d5c06f2715393211525f0eac983008
Alastair Paterson <alas.paterson@gmail.com>
2016-08-07 16:09:51 +0100

    Added vectors to Entity map
    - Switched shared_ptr values for vectors of shared_ptrs in an Entity's components map. This will allow for there to now be multiple of a single type of component (e.g. maybe several damage sources hit the entity at once).
    - Updated the entity's methods to now account for this change.
    - Added methods to return iterators to the component vectors.

--- include/entity.h
+++ include/entity.h
@@ -4,49 +4,89 @@
 #include <stdlib.h>
 #include <memory>
 #include <map>
+#include <vector>
 #include <string>
 #include <assert.h>
 #include <typeindex>
 #include "component.h"
+
+using namespace std;
 
 class Entity {
 public:
 	Entity(int _id);
 	~Entity(void);
 
-	void add_component(const std::shared_ptr<Component>& c)
+	// Add component to the entity
+	void add_component(const shared_ptr<Component> c)
 	{
-		_components[typeid(*c)] = c;
+		_components[typeid(*c)].push_back(c);
 	}
 
+	// Remove component from entity
 	template<class T>
 	void remove_component(void)
 	{
 		_components.erase(typeid(T));
 	}
 
+	// Return pointer to component
 	template<class T>
-	std::shared_ptr<T> get_component(void)
+	shared_ptr<T> get_component(void)
 	{
-		return std::static_pointer_cast<T>(_components[typeid(T)]);
+		if(has_component<T>())
+			return static_pointer_cast<T>(_components.at(typeid(T)).at(0));
+		
+		return nullptr;
 	}
 
+	// Return const pointer to const component
 	template<class T>
-	const std::shared_ptr<const T> get_component(void) const
+	const shared_ptr<const T> get_component(void) const
 	{
-		return std::static_pointer_cast<const T>(_components.at(typeid(T)));
-	}
+		if(has_component<T>())
+			return static_pointer_cast<const T>(_components.at(typeid(T)).at(0));
 
+		return nullptr;
+	}
+	
+	// Return iterator to list of componetns of type T
+	template<class T>
+	pair<typename vector<T>::iterator, typename vector<T>::iterator>
+	get_components(void) 
+	{
+		return make_pair(
+			_components[typeid(T)].begin(),
+			_components[typeid(T)].end()
+		);
+	}
+	
+	// Return const iterator to the list of componetns of type T
+	template<class T>
+	pair<typename vector<T>::const_iterator, typename vector<T>::const_iterator>
+	get_components(void) 
+    const
+	{
+		return make_pair(
+			_components.at(typeid(T)).cbegin(),
+			_components.at(typeid(T)).cend()
+		);
+	}
+	
+	// Check to see whether the entity has component T
 	template<class T>
 	bool has_component(void)
+	const
 	{
-		return _components.find(typeid(T)) != _components.end();
+		if(_components.find(typeid(T)) != _components.end() && _components.at(typeid(T)).size() > 0)
+			return true;
+		return false;
 	}
 
 private:
 
 	int _id;
-	std::map<std::type_index, std::shared_ptr<Component>> _components;
+	map<type_index, vector<shared_ptr<Component>>> _components;
 };
 
 #endif
