c95e4ebdfb1859b5bb79435c03b57ecb334e8e49
Alastair Paterson <alastair.paterson@scivisum.co.uk>
2016-08-30 10:58:30 +0100

    Reverted multiple of a component type for entities

--- include/entity.h
+++ include/entity.h
@@ -20,7 +20,7 @@ public:
 	// Add component to the entity
 	void add_component(const shared_ptr<Component> c)
 	{
-		_components[typeid(*c)].push_back(c);
+		_components[typeid(*c)] = c;
 	}
 
 	// Remove component from entity
@@ -35,7 +35,7 @@ public:
 	shared_ptr<T> get_component(void)
 	{
 		if(has_component<T>())
-			return static_pointer_cast<T>(_components.at(typeid(T)).at(0));
+			return static_pointer_cast<T>(_components.at(typeid(T)));
 		
 		return nullptr;
 	}
@@ -45,32 +45,9 @@ public:
 	const shared_ptr<const T> get_component(void) const
 	{
 		if(has_component<T>())
-			return static_pointer_cast<const T>(_components.at(typeid(T)).at(0));
+			return static_pointer_cast<const T>(_components.at(typeid(T)));
 
 		return nullptr;
-	}
-	
-	// Return iterator to list of componetns of type T
-	template<class T>
-	pair<typename vector<T>::iterator, typename vector<T>::iterator>
-	get_components(void) 
-	{
-		return make_pair(
-			_components[typeid(T)].begin(),
-			_components[typeid(T)].end()
-		);
-	}
-	
-	// Return const iterator to the list of componetns of type T
-	template<class T>
-	pair<typename vector<T>::const_iterator, typename vector<T>::const_iterator>
-	get_components(void) 
-    const
-	{
-		return make_pair(
-			_components.at(typeid(T)).cbegin(),
-			_components.at(typeid(T)).cend()
-		);
 	}
 	
 	// Check to see whether the entity has component T
@@ -78,7 +55,7 @@ public:
 	bool has_component(void)
 	const
 	{
-		if(_components.find(typeid(T)) != _components.end() && _components.at(typeid(T)).size() > 0)
+		if(_components.find(typeid(T)) != _components.end())
 			return true;
 		return false;
 	}
@@ -86,7 +63,7 @@ public:
 private:
 
 	int _id;
-	map<type_index, vector<shared_ptr<Component>>> _components;
+	map<type_index, shared_ptr<Component>> _components;
 };
 
 #endif
