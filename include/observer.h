b237eb5df3b344d90a86fc9ffb49c040a68d4fa0
Alastair Paterson <alastair.paterson@scivisum.co.uk>
2016-09-01 17:23:01 +0100

    Added observer.h for eventing

---
+++ include/observer.h
@@ -0,0 +1,48 @@
+#ifndef observer_h
+#define observer_h
+
+#include <vector>
+#include <memory>
+#include <algorithm>
+
+using namespace std;
+
+enum Event
+{
+	EVENT_ENTITY_CREATED = 0
+};
+
+class Observer
+{
+	public:
+		virtual ~Observer() {}
+		virtual void on_notify(const shared_ptr<Entity>&, Event) = 0;
+};
+
+class Subject
+{
+	public:
+		void add_observer(const shared_ptr<Observer>& observer)
+		{
+			_observers.push_back(observer);
+		}
+
+		void remove_observer(const shared_ptr<Observer>& observer)
+		{
+			remove_if(_observers.begin(), _observers.end(),  [&](shared_ptr<Observer>& sp)->bool { return &(*sp) == &(*observer); });
+		}
+
+	protected:
+		void notify(const shared_ptr<Entity>& entity, Event event)
+		{
+			for(vector<shared_ptr<Observer>>::iterator it = _observers.begin(); it != _observers.end(); it++)
+			{
+				(*it)->on_notify(entity, event);
+			}
+		}
+
+	private:
+		vector<shared_ptr<Observer>> _observers;
+};
+
+#endif
